06aac9c816f50608f0ab8da330ffc1f244ccbf4b
Fredy-SSA <56113098+Fredy-SSA@users.noreply.github.com>
2022-08-01 12:24:42 +0300

    gg

---
+++ Ziua-1/Intro.ps1
@@ -0,0 +1,50 @@
+Start-Transcript ./transcript.txt
+
+# Get PowerShell Module path
+$env:PSModulePath -split ";"
+
+invoke-item 'C:\Program Files\WindowsPowerShell\Modules'
+
+ii 'C:\Program Files\WindowsPowerShell\Modules'
+
+# Get PowerShellCOmputer Name
+$env:COMPUTERNAME
+
+Stop-Transcript 
+
+
+Get-Variable
+$PSUICulture  
+$PSVersionTable 
+
+#update latest PS version
+winget search Microsoft.PowerShell
+
+#Set Execution policy
+Get-ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+# `
+Set-ExecutionPolicy -ExecutionPolicy `
+RemoteSigned 
+
+$data = Get-Date
+$Parola = "Pa$$0rd $data"
+$Parola = 'Pa$$0rd $data'
+$Parola = "Pa`$`$0rd $data"
+
+$Parola
+
+# DIsplay folder/file info
+
+ls "C:\Users\a856239\OneDrive - Atos"
+ls "C:\Users\a856239\OneDrive - Atos" -Recurse
+
+Get-ChildItem "C:\Users\a856239\OneDrive - Atos"
+Get-ChildItem "C:\Users\a856239\OneDrive - Atos" -Recurse
+
+# open file in application
+code .\transcript.txt
+notepad .\transcript.txt
+
+
