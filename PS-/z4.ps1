ce48d3e008c8551be43bdf03cd0af2d8b941c976
Fredy-SSA <56113098+Fredy-SSA@users.noreply.github.com>
2022-08-04 13:02:03 +0300

    z4

--- PS-/z4.ps1
+++ PS-/z4.ps1
@@ -16,6 +16,49 @@ $rezultat = $datazi - $trecut
 $rezultat.days
 
 
+#liste array
+
+$lista = "prop1","prop2","prop3"
+$lista 
+$lista[1]
+$lista | gm
+$lista += "prop4"
+$lista 
+
+[array]$listav2 = "prop1"
+$listav2 | gm
+
+
+$listav3 = New-Object System.Collections.ArrayList 
+[System.Collections.ArrayList]$listav3 = "prop1","prop2"
+$listav3.Add("prop3")
+$listav3.Remove("prop3")
+$listav3.RemoveAt(1)
+$listav3
+
+$listav4=@()
+[array]$listav4="prop1","prop2"
+$listav4 += "prop3"
+$listav4[1]
+
+$listav5=@("prop1","prop2","prop3")
+$listav5 += "prop4"
+$listav5[1]
+
+# Hash table
+$utilizatori = @{"gicuta"="011111";"miticuta"="022222";"florica"="0333333"}
+$utilizatori.florica = "033333"
+$utilizatori.Add("decebal","044444")
+$utilizatori.Remove("miticuta")
+$utilizatori 
+
+$SIzeKB = @{n="SizeKb";e={$_.Length/1KB}}
+$SIzeKB 
+$SIzeMB = @{n="SizeKb";e={$_.Length/1MB}}
+$SIzeMB 
+ls c:\| ft Name, $SIzeKB, $SIzeMB 
+
+
 # Scripting
 # Unblock file
 ls -Recurse | Unblock-File
