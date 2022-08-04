af741749516cd218813a140d320c80883ec39ed4
Fredy-SSA <56113098+Fredy-SSA@users.noreply.github.com>
2022-08-04 09:03:50 +0300

    z4

---
+++ PS-/z4.ps1
@@ -0,0 +1,12 @@
+<#
+Script care calculeaza de cate zile esti pe pamant.
+Introduci datele de genu :
+Zi:01
+Luna:01
+An:1979
+
+Output:
+Te-ai nascut in data de 01/01/1979 .
+Pana acuma ai trait .... zile !
+
+#>
